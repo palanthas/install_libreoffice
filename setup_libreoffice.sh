920c95b5eeed36e5fd8e69804fee23e10269340b
palanthas <dmscholten@gmail.com>
2016-05-13 20:48:59 -0400

    Initial add to github and some minor changes for public display

---
+++ setup_libreoffice.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#	This will remove LibreOffice 4.x and install LibreOffice 5.x from tech_common
+#
+#	David Scholten	2016-05-10
+#
+#	1.0	Initial Build
+#
+
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this script as root"
+   exit 1
+
+else
+
+	#copy the install files from tech_common
+	rsync -rpt svr-fil1::tech_common/install_packages/Linux/LibreOffice/LibreOffice_5*/DEBS /home/dscholten/
+	cd /home/dscholten
+
+	#remove LibreOffice 4.x
+	apt-get -s -y purge libreoffice*
+	
+	#install LibreOffice 5.x
+	dpkg --simulate -i *.deb
+
+fi
+
+printf "\n\n\nLibreOffice has been installed!\n\n\n"
+
+exit 0
